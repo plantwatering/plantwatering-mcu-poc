06daffa9a29cab5a59281af0d91802e9612ba367
Alexandre Guyon <alex.guyon78@gmail.com>
2020-04-07 17:28:56 +0200

    tests: valve: add mock tests

--- tests/valve_test.cc
+++ tests/valve_test.cc
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "valve.h"
 #include "valve_console.h"
+#include "valve_mock.h"
 
 TEST(Valve, init_test_driver_console_fail) {
     struct valve_driver *drv = NULL;
@@ -46,4 +47,72 @@ TEST(Valve, init_test_stack_succeed) {
     EXPECT_EQ(valve_init(&v, &drv), 0);
 
     valve_console_deinit(&drv);
+}
+
+TEST(Valve, stack_test_mock) {
+    struct valve_driver drv;
+    struct valve v;
+
+    valve_mock_init(&drv);
+
+    EXPECT_EQ(valve_init(&v, &drv), 0);
+
+    valve_mock_deinit(&drv);
+}
+
+TEST(Valve, stack_test_get_value) {
+    struct valve_driver drv;
+    struct valve v;
+    bool val;
+
+    valve_mock_init(&drv);
+    valve_init(&v, &drv);
+
+    // Mocked valve is normally closed
+    EXPECT_EQ(v.get(&v, &val), 0);
+    EXPECT_EQ(val, false);
+
+    valve_mock_deinit(&drv);
+}
+
+TEST(Valve, stack_test_set_value) {
+    struct valve_driver drv;
+    struct valve v;
+    bool val;
+
+    valve_mock_init(&drv);
+    valve_init(&v, &drv);
+
+    for (int i = 0 ; i < 16; ++i) {
+        // Open valve
+        EXPECT_EQ(v.set(&v, true), 0);
+        EXPECT_EQ(v.get(&v, &val), 0);
+        EXPECT_EQ(val, true);
+        // Close valve
+        EXPECT_EQ(v.set(&v, false), 0);
+        EXPECT_EQ(v.get(&v, &val), 0);
+        EXPECT_EQ(val, false);
+    }
+
+    valve_mock_deinit(&drv);
+}
+
+void notificate_callback(const struct valve *v, bool value)
+{
+    EXPECT_EQ(value, true);
+}
+
+TEST(Valve, stack_test_notif) {
+    struct valve_driver drv;
+    struct valve v;
+
+    valve_mock_init(&drv);
+    valve_init(&v, &drv);
+
+    v.notificate = notificate_callback;
+
+    // Open valve
+    v.set(&v, true);
+
+    valve_mock_deinit(&drv);
 }
